6320ecc3f198993837378089b860bfaf57860655
TemKaa1337 <temkaaw0wpro@gmail.com>
2021-06-18 12:29:53 +0300

    fix logs

--- src/index.php
+++ src/index.php
@@ -47,7 +47,7 @@ class App
             $response = new Response($request->getChatId());
             $response->sendResponse($e->getMessage());
         } catch (Exception $e) {
-            $db->execute('INSERT INTO exception_logging (stack_trace, message, file, line, created_at) VALUES (?, ?, ?, ?, ?)', [$e->getTraceAsString(), $e->getMessage(), $e->getFile(), $e->getLine()]);
+            $db->execute('INSERT INTO exception_logging (stack_trace, message, file, line, created_at) VALUES (?, ?, ?, ?, ?)', [$e->getTraceAsString(), $e->getMessage(), $e->getFile(), $e->getLine(), date('Y-m-d H:i:s')]);
             $response = new Response($request->getChatId());
             $response->sendResponse('Случилась неизвестная ошибка, надо чекать логи((');
         }
