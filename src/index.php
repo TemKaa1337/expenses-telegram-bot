c231164da23e68a1892d9a3483e454fce3f0f093
TemKaa1337 <temkaaw0wpro@gmail.com>
2021-06-18 10:53:21 +0300

    fix database errors

--- src/index.php
+++ src/index.php
@@ -34,7 +34,7 @@ class App
             $response = new Response($request->getChatId());
             $response->sendResponse($responseMessage);
         } catch (Exception $e) {
-            $db->execute('INSERT INTO exception_logging VALUES (?, ?, ?, ?, ?)', [$e->getTraceAsString(), $e->getMessage(), $e->getFile(), $e->getLine(), date('Y-m-d H:i:s')]);
+            $db->execute('INSERT INTO exception_logging (stack_trace, message, file, line, created_at) VALUES (?, ?, ?, ?, ?)', [$e->getTraceAsString(), $e->getMessage(), $e->getFile(), $e->getLine(), date('Y-m-d H:i:s', strtotime('+3 hours'))]);
         }
     }
 }
