137c4a7c6d6fe36738447781ff12cc4b932cfb5d
TemKaa1337 <temkaaw0wpro@gmail.com>
2021-12-20 10:09:05 +0300

    fix utf8 error #4

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -41,9 +41,10 @@ class Response
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_HEADER, ['Content-Type: application/json']);
-        curl_setopt($curl, CURLOPT_HEADER, ['Content-Length: '.strlen(json_encode($data))]);
-        curl_setopt($curl, CURLOPT_HEADER, ['charset="utf-8"']);
+        throw new \Exception($data['message']);
+        // curl_setopt($curl, CURLOPT_HEADER, ['Content-Type: application/json']);
+        // curl_setopt($curl, CURLOPT_HEADER, ['Content-Length: '.strlen(json_encode($data))]);
+        // curl_setopt($curl, CURLOPT_HEADER, ['charset="utf-8"']);
           
         $response = json_decode(curl_exec($curl), true); 
           
