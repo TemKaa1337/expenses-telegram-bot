71e65dcfd02348cfb3ed1a3ce3e93702b2d196ca
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-08-21 14:56:26 +0300

    remove unsused lines

--- test_all.php
+++ test_all.php
@@ -4,7 +4,4 @@
     .\vendor\bin\phpunit tests
     .\vendor\bin\phpstan analyze src
     .\vendor\bin\phpcs src
-
-    buy: 23/31% 60$ (77.9$ 100%) 31% = 54$
-    trade: 35/45% 78$ (120$ 100%) 55% = 66$
 */
